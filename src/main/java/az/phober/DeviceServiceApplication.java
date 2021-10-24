6bde7ffa9e39d742a6ba18036314befe606f680d
Abdukhaligov <hikmat.pou@gmail.com>
2021-10-24 21:49:53 +0400

    initial commit

---
+++ src/main/java/az/phober/DeviceServiceApplication.java
@@ -0,0 +1,13 @@
+package az.phober;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeviceServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeviceServiceApplication.class, args);
+	}
+
+}
